4aae580a848701249f10f828e2980a231f4b516f
BNV4 <ruslan2804y@gmail.com>
2022-07-24 01:49:20 +0300

    update RoleController.php

--- app/Http/Controllers/RoleController.php
+++ app/Http/Controllers/RoleController.php
@@ -6,5 +6,58 @@ use Illuminate\Http\Request;
 
 class RoleController extends Controller
 {
-    //
+
+    /**
+     * Получение всех ролей
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function index(Request $request)
+    {
+
+    }
+
+    /**
+     * Создание роли
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function create(Request $request)
+    {
+
+    }
+
+    /**
+     * Получение роли
+     * @param Request $request
+     * @param integer $id
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function show(Request $request, int $id)
+    {
+
+    }
+
+    /**
+     * Обновление роли
+     * @param Request $request
+     * @param int $id
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function update(Request $request, int $id)
+    {
+
+    }
+
+    /**
+     * Удаление роли
+     * @param Request $request
+     * @param int $id
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function delete(Request $request, int $id)
+    {
+
+    }
+
 }
