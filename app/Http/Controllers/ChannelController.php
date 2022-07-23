4162b35d3dfff4cdf5cdcafe6af78a2c5f5c9a34
BNV4 <ruslan2804y@gmail.com>
2022-07-24 01:47:38 +0300

    update ChannelController.php

--- app/Http/Controllers/ChannelController.php
+++ app/Http/Controllers/ChannelController.php
@@ -6,5 +6,58 @@ use Illuminate\Http\Request;
 
 class ChannelController extends Controller
 {
-    //
+
+    /**
+     * Получение всех каналов
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function index(Request $request)
+    {
+
+    }
+
+    /**
+     * Создание канала
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function create(Request $request)
+    {
+
+    }
+
+    /**
+     * Получение канала
+     * @param Request $request
+     * @param integer $id
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function show(Request $request, int $id)
+    {
+
+    }
+
+    /**
+     * Обновление канала
+     * @param Request $request
+     * @param int $id
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function update(Request $request, int $id)
+    {
+
+    }
+
+    /**
+     * Удаление канала
+     * @param Request $request
+     * @param int $id
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function delete(Request $request, int $id)
+    {
+
+    }
+
 }
