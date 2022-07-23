79d04c822022ce5216210e92efd627b2367f96d4
BNV4 <ruslan2804y@gmail.com>
2022-07-23 22:48:27 +0300

    create Member

---
+++ database/migrations/2022_07_23_174558_create_members_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMembersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('members', function (Blueprint $table) {
+            $table->id();
+            $table->text('name');
+            $table->text('discriminator');
+            $table->text('avatar');
+            $table->text('search');
+            $table->json('roles');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('members');
+    }
+}
