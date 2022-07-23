255984d65cb2473e8a6efce68637e852458da111
BNV4 <ruslan2804y@gmail.com>
2022-07-24 00:57:28 +0300

    update api routes

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,10 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\MemberController;
+use App\Http\Controllers\RoleController;
+use App\Http\Controllers\ChannelController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +17,30 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('auth:sanctum')->group(function(){
+
+    Route::name('members.')->prefix('members')->group(function(){
+        Route::get('/', [MemberController::class, 'index'])->name('index');
+        Route::post('/', [MemberController::class, 'create'])->name('create');
+        Route::get('/{id}', [MemberController::class, 'show'])->name('show');
+        Route::put('/{id}', [MemberController::class, 'update'])->name('update');
+        Route::delete('/{id}', [MemberController::class, 'delete'])->name('delete');
+    });
+
+    Route::name('roles.')->prefix('roles')->group(function(){
+        Route::get('/', [RoleController::class, 'index'])->name('index');
+        Route::post('/', [RoleController::class, 'create'])->name('create');
+        Route::get('/{id}', [RoleController::class, 'show'])->name('show');
+        Route::put('/{id}', [RoleController::class, 'update'])->name('update');
+        Route::delete('/{id}', [RoleController::class, 'delete'])->name('delete');
+    });
+
+    Route::name('channels.')->prefix('channels')->group(function(){
+        Route::get('/', [ChannelController::class, 'index'])->name('index');
+        Route::post('/', [ChannelController::class, 'create'])->name('create');
+        Route::get('/{id}', [ChannelController::class, 'show'])->name('show');
+        Route::put('/{id}', [ChannelController::class, 'update'])->name('update');
+        Route::delete('/{id}', [ChannelController::class, 'delete'])->name('delete');
+    });
+
 });
