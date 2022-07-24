6b08e64bd32547699b78a43f547dee7cc6f7eb36
BNV4 <ruslan2804y@gmail.com>
2022-07-24 12:23:32 +0300

    update Member.php

--- app/Models/Member.php
+++ app/Models/Member.php
@@ -13,9 +13,42 @@ class Member extends Model
         'roles' => 'array',
     ];
 
-    public function generate()
+    /**
+     * Возвращает URL путь к аватарке пользователя и сохраняет его
+     * @param string|null $avatar Hesh аватарки пользователя
+     * @return string Ссылка
+     */
+    public function generateAvatar(?string $avatar = null): string
     {
+        return $this->avatar = $avatar == null
+            ? $this->getDefaultAvatarLink(substr($this->discriminator, -1))
+            : 'avatars/'.$this->id.'/'.$avatar;
+    }
 
+    /**
+     * Возвращает URL путь к дефолтному аватару Дискорда
+     * @param string $discriminator Последняя цифра дискриминатора
+     * @return string Ссылка
+     */
+    public static function getDefaultAvatarLink($discriminator): string
+    {
+        $result = 0;
+        switch($discriminator){
+            case '1': case '6': $result = 1; break;
+            case '2': case '7': $result = 2; break;
+            case '3': case '8': $result = 3; break;
+            case '4': case '9': $result = 4; break;
+        }
+        return 'embed/avatars/'.$result.'.png';
+    }
+
+    /**
+     * Возвращает текст для поиска и сохраняет его
+     * @return string
+     */
+    public function generateSearch(): string
+    {
+        return $this->search = $this->id.mb_strtolower($this->name).'#'.$this->discriminator;
     }
 
 }
