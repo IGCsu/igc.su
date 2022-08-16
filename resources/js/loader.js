0b4705f33dc037fd19975f6d61cf99307f66ef7f
BNV4 <ruslan2804y@gmail.com>
2022-08-16 15:14:13 +0300

    Optimization loader

--- resources/js/loader.js
+++ resources/js/loader.js
@@ -1,3 +1,3 @@
 document.addEventListener('DOMContentLoaded', function(){
-    document.querySelector('.loader').classList.add('loader-loaded');
+	document.querySelector('.loader').classList.add('loader-loaded');
 });
