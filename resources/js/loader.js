bd9a71642830a6510c29e39bba96fdadb0c5b150
BNV4 <ruslan2804y@gmail.com>
2022-10-02 12:54:47 +0300

    Update loader

--- resources/js/loader.js
+++ resources/js/loader.js
@@ -1,3 +1,19 @@
-document.addEventListener('DOMContentLoaded', function(){
+document.addEventListener('DOMContentLoaded', function () {
 	document.querySelector('.loader').classList.add('loader-loaded');
+	document.querySelector('.loaderIcon').classList.remove('enabled');
+});
+
+document.addEventListener('click', function (e) {
+	let href;
+
+	for (const elem of e.path) {
+		if (
+			elem instanceof Element &&
+			null !== (href = elem?.getAttribute('href'))
+		) {
+			document.querySelector('.loader').classList.remove('loader-loaded');
+			document.querySelector('.loaderIcon').classList.add('enabled');
+			return;
+		}
+	}
 });
